eea77a2b5aa6a46b2f8983b23b3f8f8ce6a31a6e
Patrick Ayer <payer@ranker.com>
2020-04-19 15:14:40 -0700

    added something don't really remember

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
         super(props);
         this.state = {
             characterList: [],
+            appState: 'start',
             create: true,
             editId: null
         };
@@ -38,6 +39,24 @@ class App extends React.Component {
         document.getElementById('editForm--npc').classList.toggle('hide');
     }
 
+    // SETTING EDIT AND CREATE STATES
+    setEditState(c, i = 0) {
+        this.setState({
+            create: c,
+            i: i == 0 ? null : i
+        });
+    }
+
+    cancelPCFormEdit() {
+        this.toggleEditMenuPCForm();
+        this.setEditState(true);
+    }
+
+    cancelNPCFormEdit() {
+        this.toggleEditMenuNPCForm();
+        this.setEditState(true);
+    }
+
     // SAVE CHARACTER FUNCTIONS
     saveCharacterData(newCharacter) {
         if (this.state.create) {
@@ -57,27 +76,17 @@ class App extends React.Component {
                     }));
                 }
             }
-            this.setState({
-                create: true,
-                editId: null
-            });
+            this.setEditState(true);
         }
-        if (newCharacter.pc) {
-            this.toggleEditMenuPCForm();
-        }
-        else {
-            this.toggleEditMenuNPCForm();
-        }
+
+        newCharacter.pc ? this.toggleEditMenuPCForm() : this.toggleEditMenuNPCForm();
     }
 
     editCharacter(characterId) {
         for (let i = 0; i < this.state.characterList.length; i++) {
             if (this.state.characterList[i].id == characterId) {
                 
-                this.setState({
-                    create: false,
-                    editId: characterId
-                });
+                this.setEditState(false, characterId);
 
                 if (this.state.characterList[i].pc) {
                     this.toggleEditMenuPCForm();
@@ -87,8 +96,57 @@ class App extends React.Component {
                     document.getElementById('editForm__dexterityInput--pc').value = this.state.characterList[i].dexterity;
                 }
 
+                else {
+                    this.toggleEditMenuNPCForm();
+                    document.getElementById('editForm__nameInput--npc').value = this.state.characterList[i].name;
+                    document.getElementById('editForm__raceInput--npc').value = this.state.characterList[i].race;
+                    document.getElementById('editForm__passivePerceptionInput--npc').value = this.state.characterList[i].passivePerception;
+                    document.getElementById('editForm__dexterityInput--npc').value = this.state.characterList[i].dexterity;
+                    document.getElementById('editForm__hitPointsInput--npc').value = this.state.characterList[i].hitPoints;
+                    document.getElementById('editForm__hitDiceInput--npc').value = this.state.characterList[i].hitDiceCount;
+                    document.getElementById('editForm__hitPointsModInput--npc').value = this.state.characterList[i].hitPointsModifier;
+                    document.getElementById('editForm__pageNumberInput--npc').value = this.state.characterList[i].pageNumber;
+                    switch(this.state.characterList[i].hitDice) {
+                        case 4:
+                            document.getElementById('editForm__d4--npc').checked = true;
+                            break;
+                        case 6:
+                            document.getElementById('editForm__d6--npc').checked = true;
+                            break;
+                        case 8:
+                            document.getElementById('editForm__d8--npc').checked = true;
+                            break;
+                        case 10:
+                            document.getElementById('editForm__d10--npc').checked = true;
+                            break;
+                        case 12:
+                            document.getElementById('editForm__d12--npc').checked = true;
+                            break;
+                        case 20:
+                            document.getElementById('editForm__d20--npc').checked = true;
+                    }
+                }
+
             }
         }
+    }
+
+    deleteCharacter(characterId) {
+
+        if (!confirm('Are you sure you want to delete this character?')) {
+            return;
+        }
+
+        for (let i = 0; i < this.state.characterList.length; i++) {
+            if (this.state.characterList[i].id == characterId) {
+                let newArr = this.state.characterList;
+                newArr.splice(i, 1);
+                this.setState({
+                    characterList: newArr
+                });
+            }
+        }
+
     }
 
     render() {
@@ -114,10 +172,10 @@ class App extends React.Component {
                         </div>
                         <PCForm header={`${this.state.create ? 'Create' : 'Edit' } PC`}
                                 saveCharacterData={this.saveCharacterData.bind(this)}
-                                hideThisForm={this.toggleEditMenuPCForm.bind(this)}/>
+                                hideThisForm={this.cancelPCFormEdit.bind(this)}/>
                         <NPCForm header={`${this.state.create ? 'Create ' : 'Edit '} NPC`}
                                  saveCharacterData={this.saveCharacterData.bind(this)}
-                                 hideThisForm={this.toggleEditMenuNPCForm.bind(this)}/>
+                                 hideThisForm={this.cancelNPCFormEdit.bind(this)}/>
                     </div>
                     <div id='characterList' className={this.state.characterList.length == 0 ? 'hide' : ''}>
                         <fieldset id='characterList__border'>
@@ -131,7 +189,8 @@ class App extends React.Component {
                                                     isUnique={c.unique}
                                                     name={c.name != '' ? c.name : c.race}
                                                     passivePerception={c.passivePerception}
-                                                    edit={this.editCharacter.bind(this)}/>
+                                                    edit={this.editCharacter.bind(this)}
+                                                    delete={this.deleteCharacter.bind(this)}/>
                                 ))}
                             </div>
                         </fieldset>
--- src/components/CharacterEntry/CharacterEntry.js
+++ src/components/CharacterEntry/CharacterEntry.js
@@ -2,12 +2,6 @@ import React from 'react';
 
 import './CharacterEntry.css';
 
-/*
-props array includes:
-isUnique { boolean } : character is unique
-name {string} : character's name
-passivePerception { integer } : character's passive perception
-*/
 class CharacterEntry extends React.Component {
 
     constructor(props) {
@@ -19,7 +13,7 @@ class CharacterEntry extends React.Component {
             <div className='characterList__character'>
                 <button className='characterList__nameBtn' disabled={true}>{this.props.isUnique ? '*' : ''}{this.props.name}</button>
                 <button className='characterList__editBtn' disabled={!this.props.create} onClick={() => (this.props.edit(this.props.characterIndex))}>Edit</button>
-                <button className='characterList__deleteBtn' disabled={!this.props.create}>Delete</button>
+                <button className='characterList__deleteBtn' disabled={!this.props.create} onClick={() => (this.props.delete(this.props.characterIndex))}>Delete</button>
                 <div className='characterList__passivePerceptionDisplay'>PP: {this.props.passivePerception}</div>
             </div>
         );
