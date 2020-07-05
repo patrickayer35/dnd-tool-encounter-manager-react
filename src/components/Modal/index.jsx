eb9f9dddc45301ea048d3ba4916052d4ff21c2f8
Patrick Ayer <payer@ranker.com>
2020-07-05 11:39:25 -0700

    save

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,16 +1,11 @@
 import React, { useState, useContext } from 'react';
 import './modal.scss';
 import sanitize from 'sanitize-filename';
-import writeJsonFile from 'write-json-file';
 import { AppContext } from '../../AppContext/index.jsx';
 
 const Modal = () => {
   const { appState, dispatch } = useContext(AppContext);
   const [filename, setFileName] = useState('');
-
-  const saveSession = async () => {
-    await writeJsonFile(`${sanitize(filename)}.json`, appState.characters);
-  }
 
   return (
     <div className="modal">
@@ -20,7 +15,7 @@ const Modal = () => {
           <label>Session name: </label>
           <input type="text" onChange={event => setFileName(event.target.value)}></input>
           <div className="btnsWrapper">
-            <button onClick={() => saveSession()}>Save</button>
+            <button>Save</button>
             <button onClick={() => dispatch({ action: 'SWITCH_VIEW', value: 'HOME' })}>Cancel</button>
           </div>
         </fieldset>
